e71bad9b5b7a347120203eb1344d7c03625591db
Halis Oy <78145526+HalisOy@users.noreply.github.com>
2024-01-22 17:45:21 +0300

    First initialize

---
+++ DatabaseExample/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DatabaseExample.Entities;
+using DatabaseExample.Repositories;
+
+Console.WriteLine("Hello, World!");
+
+ExampleDbContext db = new ExampleDbContext();
+
+User newUser = new User
+{
+    UserName = "halisoy",
+    Password = "1234",
+    FirstName = "Halis",
+    LastName = "Oy",
+    IdentificationNumber = "11223344551",
+    IsActive = true
+
+};
+
+//db.Users.Add(newUser);
+//db.Users.Remove();
+
+db.SaveChanges();
